42843192e681718497c6769d921947fa93a3e612
Henok Negatu <henoknegatu111@gmail.com>
2025-04-12 21:19:48 +0300

    feat(cli): add print_header function to display project banner
    Introduced the `print_header` function to display a styled project banner with version, description, and helpful links when the CLI starts.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,40 @@ use commands::{activator::activate, deactivator::deactiate, new::new};
 use colored::Colorize;
 
 use std::process;
+
+fn print_header() {
+    println!(
+        r#"
+{b}      ___           ___           ___           ___ 
+     /\__\         /\  \         /\  \         /\__\
+    /:/  /        /::\  \       /::\  \       /:/  /
+   /:/__/        /:/\:\  \     /:/\:\  \     /:/  / 
+  /::\__\____   /::\~\:\  \   /::\~\:\  \   /:/  /  
+ /:/\:::::\__\ /:/\:\ \:\__\ /:/\:\ \:\__\ /:/__/   
+ \/_|:|~~|~    \:\~\:\ \/__/ \:\~\:\ \/__/ \:\  \   
+    |:|  |      \:\ \:\__\    \:\ \:\__\    \:\  \  
+    |:|  |       \:\ \/__/     \:\ \/__/     \:\  \ 
+    |:|  |        \:\__\        \:\__\        \:\__\
+     \|__|         \/__/         \/__/         \/__/{y}⚡{w} {c}v0.1.0-alpha.1{w}
+
+{lg}➤ Python Project Generator on top of {lb}pip{w} 🐍 & {lb}conda{w} 📦
+{y}➤ {w}GitHub: {lb}https://github.com/HenokNegatu/keel{w}
+{y}➤ {w}License: {lb}MIT | {m}Ctrl+C{w} to exit | {m}--help{w} Options
+"#,
+        // ANSI Color Codes
+        b = "\x1b[34m",     // Blue
+        lb = "\x1b[94m",    // Light Blue
+        y = "\x1b[33m",     // Yellow
+        m = "\x1b[35m",     // Magenta
+        c = "\x1b[36m",     // Cyan
+        lg = "\x1b[92m",    // Light Green
+        w = "\x1b[0m"       // Reset
+            );
+}
+
+
 fn main() {
+    print_header();
     let cli = Cli::parse();
     match &cli.command {
         Commands::New => match new() {
